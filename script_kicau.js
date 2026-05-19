424e325d74d2d30cec33d9df215b5288eae8e927
persona6protagonist <mahaarya09@gmail.com>
2026-05-19 23:37:03 +0800

    Fondasi

---
+++ script_kicau.js
@@ -0,0 +1,88 @@
+let transaksi =[];
+
+function tambahTransaksi() {
+    const keterangan = document.getElementById('keterangan').value;
+    const jumlah = Number(document.getElementById('jumlah').value);
+    const jenis = document.getElementById('jenis').value;
+    const tanggal = document.getElementById('tanggal').value;
+
+    if(!keterangan || !jumlah || !tanggal){
+        alert('Harap Diisi');
+        return;
+    }
+
+    const data = {
+        id: Date.now(),
+        keterangan,
+        jumlah,
+        jenis,
+        tanggal
+    };
+
+    transaksi.push(data);
+
+    tampilkanTransaksi();
+    hitungSaldo();
+    bersihkanForm();
+}
+
+function tampilkanTransaksi(){
+    const tbody = document.getElementById('tabel-transaksi');
+    tbody.innerHTML='';
+
+    transaksi.forEach(function(item){
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${item.tanggal}</td>
+        <td>${item.keterangan}</td>
+        <td>
+            <span class="badge ${item.jenis === 'masuk' ? 'badge-in' : 'badge-out'} ">
+                ${item.jenis === 'masuk' ? 'Pemasukan' : 'Pengeluaran'}
+            </span>
+        </td>
+        <td class="${item.jenis === 'masuk' ? 'income' : 'expense'}">
+        ${item.jenis === 'masuk' ? '+' : '-'} Rp ${item.jumlah.toLocaleString('id-ID')}
+        </td>
+        <td>
+            <button onclick="hapusTransaksi(${item.id})">Hapus</button>
+        </td>
+        `;
+        tbody.appendChild(row);
+
+    });
+}
+
+function hitungSaldo() {
+  let totalMasuk = 0;
+  let totalKeluar = 0;
+
+  transaksi.forEach(function(item) {
+    if (item.jenis === 'masuk') {
+      totalMasuk += item.jumlah;
+    } else {
+      totalKeluar += item.jumlah;
+    }
+  });
+
+  const saldo = totalMasuk - totalKeluar;
+
+  document.getElementById('total-masuk').textContent = 'Rp ' + totalMasuk.toLocaleString('id-ID');
+  document.getElementById('total-keluar').textContent = 'Rp ' + totalKeluar.toLocaleString('id-ID');
+  document.getElementById('saldo').textContent = 'Rp ' + saldo.toLocaleString('id-ID');
+}
+
+function hapusTransaksi(id) {
+  // Filter: simpan semua kecuali yang id-nya sama
+  transaksi = transaksi.filter(function(item) {
+    return item.id !== id;
+  });
+
+  tampilkanTransaksi();
+  hitungSaldo();
+}
+
+function bersihkanForm() {
+  document.getElementById('keterangan').value = '';
+  document.getElementById('jumlah').value = '';
+  document.getElementById('tanggal').value = '';
+}
